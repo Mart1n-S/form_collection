4eefaa3246b27833e5482fd39741830a5650dbd1
Martin <martinsimongo@gmail.com>
2025-02-12 11:10:29 +0100

    check année bissextiles coté front car le back le fait par défaut

--- src/Form/TestFormType.php
+++ src/Form/TestFormType.php
@@ -2,9 +2,11 @@
 
 namespace App\Form;
 
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class TestFormType extends AbstractType
@@ -12,6 +14,12 @@ class TestFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('nom', TextType::class)
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'html5' => false,
+                'format' => 'dd/MM/yyyy',
+                'attr' => ['class' => 'date'],
+            ])
             ->add('num', TextType::class);
     }
 
